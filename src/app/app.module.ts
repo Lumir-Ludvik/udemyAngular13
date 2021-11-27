b23d4609dae59099fbe5ae02ec2b42ce39c7e8d7
Lumír Ludvík <lumirludvik@email.cz>
2021-11-27 17:01:52 +0100

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from "./UI/header/header.component";
+import {RecipesComponent} from './components/recipies/recipes.component';
+import { RecipeListComponent } from './components/recipies/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './components/recipies/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './components/recipies/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './components/shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +29,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
